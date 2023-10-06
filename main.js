da63d39982df206d062e919e271fb838f2c0d13f
KrishKashiwala <krishkashiwala@gmail.com>
2023-10-06 19:26:58 +0530

    routes seperated for dev and prod

--- main.js
+++ main.js
@@ -3,49 +3,57 @@ import AWS from "./aws-config.js";
 const app = express();
 
 const codepipeline = new AWS.CodePipeline();
-app.post("/pipelines", async (req, res) => {
-  //top
 
-  async function triggerPipeline(pipelineName) {
-    const params = {
-      name: pipelineName,
-    };
-
-    try {
-      const response = await codepipeline
-        .startPipelineExecution(params)
-        .promise();
-      console.log(
-        `Pipeline execution started successfully for ${pipelineName}:`,
-        response
-      );
-    } catch (error) {
-      console.error(
-        `Error starting pipeline execution for ${pipelineName}:`,
-        error
-      );
-      throw error;
-    }
-  }
-  //bot
+//development
+app.post("/development", async (req, res) => {
+  const params = {
+    name: "skillc-frontend",
+  };
 
   try {
-    await triggerPipeline("skillc-frontend");
-    // console.log("test: ", process.env.AWS_DEV_PIPE_NAME);
-
-    await triggerPipeline("skillac_frontend-production");
-
+    const response = await codepipeline
+      .startPipelineExecution(params)
+      .promise();
+    console.log(
+      `Pipeline execution started successfully for ${params.name}:`,
+      response
+    );
     res.send("Webhook received and both pipelines triggered successfully.");
   } catch (error) {
-    console.error("Error triggering pipelines:", error);
+    console.error(
+      `Error starting pipeline execution for ${params.name}:`,
+      error
+    );
 
     res.status = 500;
     res.send("Error triggering pipelines");
   }
+});
 
-  //   const response = await triggerCodePipeline();
-  //   console.log("response: ", response);
-  //   return res.json(response);
+//production
+app.post("/production", async (req, res) => {
+  const params = {
+    name: "skillac_frontend-production",
+  };
+
+  try {
+    const response = await codepipeline
+      .startPipelineExecution(params)
+      .promise();
+    console.log(
+      `Pipeline execution started successfully for ${params.name}:`,
+      response
+    );
+    res.send("Webhook received and both pipelines triggered successfully.");
+  } catch (error) {
+    console.error(
+      `Error starting pipeline execution for ${params.name}:`,
+      error
+    );
+
+    res.status = 500;
+    res.send("Error triggering pipelines");
+  }
 });
 
 app.listen(3000, () => console.log("server started on port 3000"));
